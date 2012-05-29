a80b03e489fbf70c15f7462e0793a12c3742fc3f
Prof1983 <prof1983@yandex.ru>
2012-05-29 07:59:29 +0600

    Added ACoreLibQt project

---
+++ Source/ACoreLibQt/main.c
@@ -0,0 +1,15 @@
+/* ACoreLib
+Author(Prof1983 prof1983@ya.ru)
+Created(28.05.2012)
+LastMod(28.05.2012)
+Version(0.0.0)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
+
