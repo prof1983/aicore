45560b58946b50b13427f248e8aafd03a600d326
Prof1983 <prof1983@yandex.ru>
2012-05-29 08:00:35 +0600

    Added ACoreLib project

---
+++ Source/ACoreLib/ACoreLibMain.c
@@ -0,0 +1,14 @@
+/* ACoreLib
+Author(Prof1983 prof1983@ya.ru)
+Created(28.05.2012)
+LastMod(28.05.2012)
+Version(0.0.0)
+*/
+
+#include <windows.h>
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved)
+{
+	return 1;
+}
