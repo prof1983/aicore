0fd4d86764750d17e6837130982bd633267027c6
Prof1983 <prof1983@yandex.ru>
2012-05-29 10:00:21 +0600

    Added compiledynamic target in ACoreLib/Makefile

---
+++ Source/ACoreLib/ACoreLibTest_dynamic.c
@@ -0,0 +1,44 @@
+// ACoreLibTest_dynamic.c -- demonstrate dynamic loading and use of the "hello" routine
+
+// Need dlfcn.h for the routines to dynamically load libraries
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Note that we don't have to include "libhello.h".
+     However, we do need to specify something related;
+        we need to specify a type that will hold the value
+           we're going to get from dlsym(). */
+
+/* The type "simple_demo_function" describes a function that
+     takes no arguments, and returns no value: */
+
+typedef void (*simple_demo_function)(void);
+
+int main(void) {
+	const char *error;
+	void *module;
+	simple_demo_function demo_function;
+
+	// Load dynamically loaded library.
+	module = dlopen("ACoreLib.so", RTLD_LAZY);
+	if (!module) {
+		fprintf(stderr, "Couldn't open ACoreLib.so: %s\n");
+		exit(1);
+	}
+
+	// Get symbol
+	// dlerror();
+	demo_function = dlsym(module, "hello");
+	/*
+	if ((error = dlerror())) {
+		fprintf(stderr, "Couldn't find hello: %s\n", error);
+		exit(1);
+	}
+	*/
+	
+	(*demo_function)();
+
+	dlclose(module);
+	return 0;
+}
